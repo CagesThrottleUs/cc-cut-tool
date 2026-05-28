954a6cfb024751d3fc759f87d2c9e426fc0bfcfd
CagesThrottleUs <manstein.felix@gmail.com>
2026-05-28 13:37:03 +0530

    test(sp01): add CutOptions unit tests

--- tests/cut/options_test.cpp
+++ tests/cut/options_test.cpp
@@ -1,1 +1,26 @@
-// placeholder
+#include <gtest/gtest.h>
+#include "cut/options.hpp"
+
+// TC-REQ003-01
+TEST(CutOptionsTest, DefaultInit) {
+    CutOptions opts;
+    EXPECT_EQ(opts.mode, CutMode::FIELD);
+    EXPECT_EQ(opts.delim, std::nullopt);
+    EXPECT_FALSE(opts.suppress);
+    EXPECT_FALSE(opts.no_split);
+}
+
+// TC-REQ003-02
+TEST(CutOptionsTest, SetDelim) {
+    CutOptions opts;
+    opts.delim = ',';
+    EXPECT_TRUE(opts.delim.has_value());
+    EXPECT_EQ(opts.delim.value(), ',');
+}
+
+// TC-REQ003-03
+TEST(CutOptionsTest, SetSuppress) {
+    CutOptions opts;
+    opts.suppress = true;
+    EXPECT_TRUE(opts.suppress);
+}
