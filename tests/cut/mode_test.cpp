620bb3026fcf3ab39edcfab586d6e697e0cc9fe6
CagesThrottleUs <manstein.felix@gmail.com>
2026-05-28 13:31:06 +0530

    test(sp01): add CutMode unit tests

--- tests/cut/mode_test.cpp
+++ tests/cut/mode_test.cpp
@@ -1,1 +1,14 @@
-// placeholder
+#include <gtest/gtest.h>
+#include "cut/mode.hpp"
+
+// TC-REQ001-01, TC-REQ001-02
+static_assert(CutMode::BYTE != CutMode::CHARACTER);
+static_assert(CutMode::CHARACTER != CutMode::FIELD);
+
+// TC-REQ001-03
+static_assert(sizeof(CutMode) == 1);
+
+TEST(CutModeTest, DistinctValues) {
+    EXPECT_NE(CutMode::BYTE, CutMode::CHARACTER);
+    EXPECT_NE(CutMode::CHARACTER, CutMode::FIELD);
+}
